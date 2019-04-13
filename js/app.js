52c4242264e0d6412f52bbb19f1d21d7b14272e1
Abdullah <aarokan@gmail.com>
2019-04-14 00:34:15 +0300

    feat: Add openCard function

--- js/app.js
+++ js/app.js
@@ -36,7 +36,9 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+ function openCard (evt) {
+   evt.target.classList.add('open', 'show');
+ }
+
 const deck = document.querySelector('.deck');
-deck.addEventListener('click', function(evt) {
-  evt.target.classList.add('open', 'show');
-});
+deck.addEventListener('click', openCard);
