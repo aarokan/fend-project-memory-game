03ee8144b9fd06c6a5448d1d9257e0a869f6162a
Abdullah <aarokan@gmail.com>
2019-04-30 15:09:51 +0300

    refactor: Use concat to duplicate the icons

--- js/app.js
+++ js/app.js
@@ -4,22 +4,17 @@
 
 let listOfCards = [
     "fa fa-anchor",
-    "fa fa-anchor",
-    "fa fa-bicycle",
     "fa fa-bicycle",
     "fa fa-bolt",
-    "fa fa-bolt",
-    "fa fa-bomb",
     "fa fa-bomb",
     "fa fa-cube",
-    "fa fa-cube",
-    "fa fa-diamond",
     "fa fa-diamond",
     "fa fa-leaf",
-    "fa fa-leaf",
-    "fa fa-paper-plane-o",
     "fa fa-paper-plane-o"
 ];
+
+// Use concat to duplicate the icons
+listOfCards = listOfCards.concat(listOfCards);
 
 
 /*
